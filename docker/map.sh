dc22fe78a5a7dd21a4e237642c4a31ee81d35fa6
ctjutting <jutting.c@gmail.com>
2026-07-11 16:16:45 -0500

    text message fixes

--- docker/map.sh
+++ docker/map.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Running migrations"
-npx prisma migrate dev
+npx prisma migrate deploy
+
+echo "Generating Prisma client"
+npx prisma generate
 
 echo "Starting map ui"
 exec node src/index.js ${MAP_OPTS}
